7fd02a830c37e9a8598e36d77898b1ea39b7fd7a
Michael Brown <m.overtonbrown@gmail.com>
2022-10-06 21:15:47 -0700

    clean up fmod music structure

--- Assets/Scripts/Audio/AmbienceManager.cs
+++ Assets/Scripts/Audio/AmbienceManager.cs
@@ -8,11 +8,19 @@ public class AmbienceManager : MonoBehaviour
     [SerializeField] private bool playAmbience;
     [SerializeField] private string ambienceEventName;
     EventInstance ambienceInstance;
-    [SerializeField] private bool playMusic;
-    [SerializeField] private string musicEventName;
-    EventInstance musicInstance;
+    // [SerializeField] private string musicEventName;
+    // EventInstance musicInstance;
     [SerializeField] private string trafficEventName;
     EventInstance trafficInstance;
+
+    // Music Events
+    [SerializeField] private bool playMusic;
+    [SerializeField] private string technoMusicEventName;
+    EventInstance technoMusic;
+    [SerializeField] private string houseMusicEventName;
+    EventInstance houseMusic;
+    [SerializeField] private string weirdMusicEventName;
+    EventInstance weirdMusic;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +28,35 @@ public class AmbienceManager : MonoBehaviour
         ambienceInstance = FMODUnity.RuntimeManager.CreateInstance("event:/" + ambienceEventName);
         if(playAmbience) ambienceInstance.start();
 
-        musicInstance = FMODUnity.RuntimeManager.CreateInstance("event:/" + musicEventName);
-        if(playMusic) musicInstance.start();
-
         trafficInstance = FMODUnity.RuntimeManager.CreateInstance("event:/" + trafficEventName);
         trafficInstance.start();
+
+        technoMusic = FMODUnity.RuntimeManager.CreateInstance("event:/" + technoMusicEventName);
+        if(playMusic) technoMusic.start();
+
+        houseMusic = FMODUnity.RuntimeManager.CreateInstance("event:/" + houseMusicEventName);
+        if(playMusic) houseMusic.start();
+
+        weirdMusic = FMODUnity.RuntimeManager.CreateInstance("event:/" + weirdMusicEventName);
+        if(playMusic) weirdMusic.start();
     }
 
     public void SetMusicParameter(string parameter, float value) {
-        musicInstance.setParameterByName(parameter, value);
+        switch(parameter) 
+        {
+            case "technoVolume": 
+                technoMusic.setParameterByName(parameter, value);
+                break;
+            case "houseVolume": 
+                houseMusic.setParameterByName(parameter, value);
+                break;
+            case "weirdVolume": 
+                weirdMusic.setParameterByName(parameter, value);
+                break;
+            default:
+                Debug.Log("ERROR SETTING MUSIC PARAMETER");
+                break;
+        }
     }
 
     public void SetCrowdParameter(string parameter, float value) {
