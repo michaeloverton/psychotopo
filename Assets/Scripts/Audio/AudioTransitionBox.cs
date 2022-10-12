012462c8c7116a7862602a08a01a1f2936a0f712
Michael Brown <m.overtonbrown@gmail.com>
2022-10-11 20:14:15 -0700

    add transitions over portals

--- Assets/Scripts/Audio/AudioTransitionBox.cs
+++ Assets/Scripts/Audio/AudioTransitionBox.cs
@@ -14,11 +14,14 @@ public class AudioTransitionBox : MonoBehaviour
     [SerializeField] bool modulateTraffic;
     [SerializeField] bool modulateMusic;
     [SerializeField] bool xModulation;
+    [SerializeField] bool yModulation;
     private BoxCollider collider;
     private float minZ;
     private float maxZ;
     private float minX;
     private float maxX;
+    private float minY;
+    private float maxY;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +32,9 @@ public class AudioTransitionBox : MonoBehaviour
 
         minX = transform.position.x - collider.size.x/2;
         maxX = transform.position.x + collider.size.x/2;
+
+        minY = transform.position.y - collider.size.y/2;
+        maxY = transform.position.y + collider.size.y/2;
     }
 
     void OnTriggerStay(Collider other)
@@ -37,6 +43,11 @@ public class AudioTransitionBox : MonoBehaviour
         if(xModulation) {
             paramValue = Utility.Remap(other.transform.position.x, minX, maxX, initialValue, maxValue);
             if(invert) paramValue = Utility.Remap(other.transform.position.x, minX, maxX, maxValue, initialValue);
+        }
+        else if(yModulation)
+        {
+            paramValue = Utility.Remap(other.transform.position.y, minY, maxY, initialValue, maxValue);
+            if(invert) paramValue = Utility.Remap(other.transform.position.y, minY, maxY, maxValue, initialValue);
         }
         else
         {
