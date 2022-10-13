fe27872cce767b50cd1afe06f85ff643d81349ec
Michael Brown <m.overtonbrown@gmail.com>
2022-10-12 22:23:53 -0700

    controlling text

--- Assets/Scripts/TextDisplayTrigger.cs
+++ Assets/Scripts/TextDisplayTrigger.cs
@@ -6,14 +6,28 @@ public class TextDisplayTrigger : MonoBehaviour
 {
     [SerializeField] private TextManager textManager;
     [SerializeField] private string text;
+    private string currentText;
+    private int currentTextIndex = 1;
+    [SerializeField] private float displaySpeed;
+    private float timer = 0;
 
     void OnTriggerStay(Collider other)
     {
-        textManager.ShowText(text);
+        timer += Time.deltaTime;
+        if(timer > displaySpeed && currentTextIndex != text.Length+1)
+        {
+            currentText = text.Substring(0, currentTextIndex);
+            textManager.ShowText(currentText);
+            currentTextIndex++;
+            timer = 0;
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         textManager.HideText();
+        currentText = "";
+        currentTextIndex = 0;
+        timer = 0;
     }
 }
